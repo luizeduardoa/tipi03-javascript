231dfadbb43c60ad97502e5bbe062bf4bc0082ab
LUIZ EDUARDO SANTOS DE ALBUQUERQUE <luiz.esalbuquerque@senacsp.edu.br>
2026-03-03 11:56:36 -0300

    Att

--- 08-loops/script.js
+++ 08-loops/script.js
@@ -39,3 +39,83 @@ do{
     console.log(n);
     n++;
 }  while(n <= 10);
+
+//break
+let contadors  = 0;
+
+while (contadors < 10){
+    console.log("Contador: " + contadors);
+
+// Interrompe o loop se o contador for igual a 5
+
+    if(contadors === 5){
+        break;
+    }
+
+    contadors++;
+    }
+
+
+// for 
+
+for (i = 1 ; i <= 5; i++) {
+    console.log(i);
+}
+
+let j = 5;
+
+for(j; j > 2; j--){
+    console.log(j);
+}
+
+
+// continue 
+
+for (let k = 1; k <= 7; k++){
+    //Pula a impressão do número 5
+    if (k === 5) {
+        continue;                                                                                                                            
+    }
+
+    console.log(k);
+} 
+
+// array
+
+const nomes = ["João", "Maria", "José"];
+
+console.log(nomes);
+console.log(nomes [0]);
+console.log(nomes [1]);
+console.log(nomes [2]);
+console.log(nomes [3]); // essa posiçaõ não existe (undefined)
+
+
+    //percorrendo um array com loop for 
+
+    const listNome = ["João", "Maria", "José"];
+
+    for(let p = 0; p < listNome.length; p++){
+        console.log(listNome[p]);
+    
+    }
+
+    //percorrendo um array com loop forEach
+
+    listNome.forEach(function(nome){
+        console.log(nome)
+    });
+
+    //mais moderno - forEach com arrow fuction
+
+    listNome.forEach(nome => {
+        console.log(nome);
+    })
+
+    //nome e index com forEach 
+    
+    listNome.forEach((nome, indice) => {
+        console.log(indice, nome);
+    })
+
+    
---
+++ 09-array/script.js
@@ -0,0 +1,24 @@
+
+const nomes = ["João", "Maria", "José"];
+
+console.log(nomes);
+console.log(nomes [0]);
+console.log(nomes [1]);
+console.log(nomes [2]);
+console.log(nomes [3]); // essa posiçaõ não existe (undefined)
+
+
+
+//Adicionando um elemento no array 
+
+nomes.push("Beatriz");
+console.log(nomes);
+
+//Removendo o ultimo elemento do array
+
+nomes.pop();
+console.log("Beatriz foi removida " + nomes);
+
+//Alterar um elemento do array 
+nomes[0] = "João Pedro";
+console.log(nomes);
